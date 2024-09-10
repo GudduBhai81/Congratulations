90e23d70bdf563692e29b42f7c0b87114926fecb
GudduBhai81 <306botala@gmail.com>
2024-09-10 22:35:18 +0500

    Add files via upload

---
+++ Congratulationscreen.py
@@ -0,0 +1,9 @@
+#correct answer
+answer = 3 + 6
+#ask the question
+userinput = int(input("What is 3+6 ? "))
+#Answer correct or not
+if answer == userinput:
+    print("Congratulations. Your answer is correct.")
+else:
+        print("Your answer is incorrect. The corect answer is", 9)
